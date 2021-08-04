a563632e29585932620170d8bb9f98a0dbfef79a
tschibly <graflniklas@gmail.com>
2021-08-04 02:14:16 +0200

    sneaker boy
    sneakerboy
    ib8 9g8
    biubi vuvt ttt

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ const navSlide = () => {
             if(link.style.animation){
                 link.style.animation = '';
             } else {
-                link.style.animation = `navLinkFade 0.5s ease forwards ${index / 12 + 0.5}s`;
+                link.style.animation = `navLinkFade 0.5s ease forwards ${index / 20 + 0.2}s`;
             }    
         });
         //Burger Animation
@@ -26,3 +26,47 @@ const navSlide = () => {
 }
 
 navSlide();
+
+const carouselSlide = document.querySelector('.carousel-slide');
+const carouselImages = document.querySelectorAll('.carousel-slide img');
+
+//Buttons
+const prevBtn = document.querySelector('#prevBtn');
+const nextBtn = document.querySelector('#nextBtn');
+
+//counter
+let counter1 = 1;
+const size = carouselImages[0].clientWidth;
+
+carouselSlide.style.transform = 'translateX(' + (-size * counter1) + 'px)';
+
+nextBtn.addEventListener('click',()=>{
+    if (counter1 >= carouselImages.length -1) return;
+    carouselSlide.style.transition = "transform 0.4s ease-in-out";
+    counter1++;
+    carouselSlide.style.transform = 'translateX(' + (-size * counter1) + 'px)';
+
+});
+
+prevBtn.addEventListener('click',()=>{
+    if (counter1 <= 0) return;
+    carouselSlide.style.transition = "transform 0.4s ease-in-out";
+    counter1--;
+    carouselSlide.style.transform = 'translateX(' + (-size * counter1) + 'px)';
+
+});
+
+carouselSlide.addEventListener('transitionend', ()=>{
+    if (carouselImages[counter].id === 'lastClone') {
+        carouselSlide.style.transition = "none";
+        counter1 = carouselImages.length -2 ;
+        carouselSlide.style.transform = 'translateX(' + (-size * counter1) + 'px)';
+
+    }
+    if (carouselImages[counter1].id === 'firstClone') {
+        carouselSlide.style.transition = "none";
+        counter1 = carouselImages.length - counter1 ;
+        carouselSlide.style.transform = 'translateX(' + (-size * counter1) + 'px)';
+
+    }
+});
